20c5c440fbbd6a6c27b96148b5c57825c3cdb0af
badaix <johannes.pohl@badaix.de>
2023-02-03 11:47:37 +0100

    Update oboe to 1.7.0

--- build_android.sh
+++ build_android.sh
@@ -198,7 +198,7 @@ cd ${BASEDIR}
 ./make_aar.sh build/aar/ ogg 1.3.5 ./build/android/ libogg.a ogg
 ./make_aar.sh build/aar/ opus 1.1.2 ./build/android/ libopus.a opus
 ./make_aar.sh build/aar/ tremor 1.0.0 ./build/android/ libvorbisidec.a tremor
-./make_aar.sh build/aar/ oboe 1.6.1 ./build/android/ liboboe.a oboe
+./make_aar.sh build/aar/ oboe 1.7.0 ./build/android/ liboboe.a oboe
 ./make_aar.sh build/aar/ soxr 0.1.3 ./build/android/ libsoxr.a soxr.h
 ./make_aar.sh build/aar/ vorbis 1.3.7 ./build/android/ libvorbis.a vorbis
 ./make_aar.sh build/aar/ boost 1.81.0 ./build/android/ "" boost_1_81_0/boost
